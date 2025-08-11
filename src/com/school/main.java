010db4c94087814d0c507d0dab55a6a4002d20b3
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-08-11 11:13:48 +0530

    Completed part-01

--- src/com/school/main.java
+++ src/com/school/main.java
@@ -3,8 +3,7 @@ package com.school;
 public class main {
     public static void main(String[] args){
         System.out.println("Welcome to the school attendence management system.");
-
-        System.out.println("Session 1: Project setupand orientation complete.");
+   System.out.println("Session 1: Project setupand orientation complete.");
     }
     
 }
